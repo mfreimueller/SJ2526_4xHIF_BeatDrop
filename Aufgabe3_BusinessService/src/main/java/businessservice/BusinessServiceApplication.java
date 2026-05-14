74618547809f6b88dfd59af127da42f0d4800856
Michael Freimueller <michael@mfreimueller.com>
2026-05-14 08:59:16 +0200

    add and configure submodules for services and rest api

---
+++ Aufgabe3_BusinessService/src/main/java/businessservice/BusinessServiceApplication.java
@@ -0,0 +1,13 @@
+package businessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BusinessServiceApplication.class, args);
+    }
+
+}
